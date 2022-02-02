bb68f1e15420f076969634a9d266c7eedf49c05a
MD <mayankdwivedi@funngage.com>
2022-02-02 19:47:26 +0530

    Added News Api

--- Container/MainContainer.tsx
+++ Container/MainContainer.tsx
@@ -1,7 +1,9 @@
-import { AutoComplete, Card, Col, Input, Row, Tag } from "antd";
+import { AutoComplete, Button, Card, Col, Input, Row, Tag } from "antd";
 import Meta from "antd/lib/card/Meta";
+import Layout, { Header } from "antd/lib/layout/layout";
 import debounce from "lodash/debounce";
 import moment from "moment";
+import Image from "next/image";
 import react, { useEffect, useState } from "react";
 import { useFetchCityRecordsQuery } from "../Features/AirQuality-api-slice";
 import { useFetchNewsQuery } from "../Features/news-slice";
@@ -11,23 +13,67 @@ const gridStyle = {
 };
 const { Search } = Input;
 const MainContainer = () => {
-  const [city, setCity] = useState("Delhi");
-  const { data, isFetching } = useFetchNewsQuery();
+  const [newsLang, setNewsLang] = useState("en");
+  const { data, isFetching } = useFetchNewsQuery(newsLang);
 
   const onSearch = (value) => {
     console.log(value);
-    setCity(value);
+    setNewsLang(value);
   };
   return (
     <>
-      <Row>
-        {isFetching
-          ? "Loading"
-          : data.flatMap((ele) => {
+      <Header
+        style={{
+          backgroundColor: "white",
+          position: "fixed",
+          width: "100%",
+          height: 50,
+          zIndex: 1,
+          boxShadow: "0 3px 5px rgba(57, 63, 72, 0.3)",
+        }}
+      >
+        <Row>
+          <Col>NEWS</Col>
+          <Col>
+            <Button
+              type="primary"
+              onClick={() => setNewsLang(newsLang == "hi" ? "en" : "hi")}
+            >
+              {newsLang == "en" ? "Hindi News" : "English News"}
+            </Button>
+          </Col>
+        </Row>
+      </Header>
+      <Layout className="main-div">
+        <Row justify="center" style={{ alignContent: "center", marginTop: 60 }}>
+          {isFetching ? (
+            // eslint-disable-next-line jsx-a11y/alt-text
+            <div style={{ width: "100%", height: "100rem" }}>
+              <Image
+                src={"/loading.gif"}
+                width={150}
+                height={150}
+                alt="loader"
+              />
+            </div>
+          ) : (
+            data.flatMap((ele) => {
               return ele.items.flatMap((news) => {
                 return (
-                  <Col sm={24} xs={24} xl={6} xxl={4} md={8}>
-                    <Card hoverable style={{ width: 300, marginBottom: 10 }}>
+                  <Col
+                    style={{ textAlign: "center", margin: 5 }}
+                    sm={24}
+                    xs={24}
+                    xl={6}
+                    xxl={4}
+                    md={8}
+                  >
+                    <Card
+                      hoverable
+                      style={{
+                        marginBottom: 10,
+                      }}
+                    >
                       <h3>{news.title}</h3>
                       <Row>
                         <Col>
@@ -53,8 +99,10 @@ const MainContainer = () => {
                   </Col>
                 );
               });
-            })}
-      </Row>
+            })
+          )}
+        </Row>
+      </Layout>
     </>
   );
 };
