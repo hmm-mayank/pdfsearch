2c243512fc6ebe84d505c2f348a3bc70ac2a84e8
mayank dwivedi <mayankanildwivedi@gmail.com>
2022-02-05 01:18:50 +0530

    fix type

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -2,7 +2,38 @@ import React from "react";
 
 import axios from "axios";
 import { UiFileInputButton } from "../Container/UiFileInputButton";
+import { Table } from "antd";
+import { useFetchLookupListQuery } from "../Features/lookup-slice";
+
+const columns = [
+  {
+    title: "lrn",
+    dataIndex: "lrn",
+    key: "lrn",
+  },
+  {
+    title: "rate center",
+    dataIndex: "ratecenter",
+    key: "ratecenter",
+  },
+  {
+    title: "network",
+    dataIndex: "network",
+    key: "network",
+  },
+  {
+    title: "country",
+    key: "country",
+    dataIndex: "country",
+  },
+  {
+    title: "delay",
+    key: "delay",
+    dataIndex: "delay",
+  },
+];
 const UploadFile = () => {
+  let da = useFetchLookupListQuery();
   const onChange = async (formData) => {
     const config = {
       headers: { "content-type": "multipart/form-data" },
@@ -20,11 +51,17 @@ const UploadFile = () => {
   };
 
   return (
-    <UiFileInputButton
-      label="Upload Single File"
-      uploadFileName="theFiles"
-      onChange={onChange}
-    />
+    <>
+      <UiFileInputButton
+        label="Upload Single File"
+        uploadFileName="theFiles"
+        onChange={onChange}
+      />
+
+      {da.isFetching === false && (
+        <Table dataSource={da.data} columns={columns} />
+      )}
+    </>
   );
 };
 
